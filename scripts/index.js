be26d01ce38232276e606f575b8bc82c7a14c3e5
DJ-JR30 <djtddy@hotmail.com>
2021-07-17 02:26:49 -0400

    init

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+function Show_AboutMe() {
+  document.getElementById("about-me-div").style.display = "block";
+  document.getElementById("about-me-div").style.visibility = "visible";
+}
+
+
+
+function Hide_AboutMe() {
+  document.getElementById("about-me-div").style.display = "hidden";
+  document.getElementById("about-me-div").style.visibility = "hidden";
+}
+
