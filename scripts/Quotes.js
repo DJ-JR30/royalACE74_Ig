4475ab08d5dd5efae7e5e79d8bab186994dd1fad
DJ-JR30 <djtddy@hotmail.com>
2021-10-20 00:13:42 -0400

    new

--- scripts/Quotes.js
+++ scripts/Quotes.js
@@ -1,10 +1,91 @@
 var PageNum = document.getElementById('PageNum');
-let Page_Current_Num = PageNum.innerHTML;
+
 
 
 
 // Pages
 var quotesPage1 = document.getElementById('quotesPage1');
+var quotesPage2 = document.getElementById('quotesPage2');
+var quotesPage3 = document.getElementById('quotesPage3');
+
+
+
+
+
+
+
+function PageChanger(Dirction) {
+  if(Dirction = 'Back') {
+    if(PageNum.innerHTML == 'Page 1') {
+      Page3()
+    }else{
+      if(PageNum.innerHTML == 'Page 2') {
+        Page1();
+      }else{
+        if (PageNum.innerHTML == 'Page 3') {
+          Page2();
+        }
+      }
+    }
+  }else{
+    if(PageNum.innerHTML == 'Page 1') {
+      Page2();
+    }else{
+      if(PageNum.innerHTML == 'Page 2') {
+        Page3();
+      }else{
+        if (PageNum.innerHTML == 'Page 3') {
+          Page1();
+        }
+      }
+    }
+  }
+}
+
+
+
+
+
+function Page1() {
+  HideAllPages();
+  PageNum.innerHTML.innerHTML = 'Page 1';
+  quotesPage1.style.display = 'block';
+  quotesPage1.style.visibility = 'visible';
+}
+
+function Page2() {
+  HideAllPages();
+  PageNum.innerHTML.innerHTML = 'Page 2';
+  quotesPage2.style.display = 'block';
+  quotesPage2.style.visibility = 'visible';
+}
+
+function Page3() {
+  HideAllPages();
+  PageNum.innerHTML.innerHTML = 'Page 3';
+  quotesPage3.style.display = 'block';
+  quotesPage3.style.visibility = 'visible';
+}
+
+
+
+
+
+
+
+function HideAllPages() {
+  quotesPage1.style.display = 'none';
+  quotesPage1.style.visibility = 'hidden';
+  
+  quotesPage2.style.display = 'none';
+  quotesPage2.style.visibility = 'hidden';
+  
+  quotesPage3.style.display = 'none';
+  quotesPage3.style.visibility = 'hidden';
+}
+
+
+
 
 
 
@@ -15,43 +96,4 @@ function BackPressed() {
 function FrowardPressed() {
   PageChanger('Foward');
 }
-
-
-
-function PageChanger(Dirction) {
-  if(Dirction = 'Back') {
-    if(Page_Current_Num == 1) {
-      // Back
-    }
-  }else{
-    if(Page_Current_Num == 1) {
-      // Forward
-    }
-  }
-}
-
-
-
-
-
-function Page1() {
-  HideAllPages()
-  quotesPage1.display.display = 'block';
-  quotesPage1.display.visibility = 'visible';
-}
-
-
-
-
-
-
-
-function HideAllPages() {
-  quotesPage1.display.display = 'none';
-  quotesPage1.display.visibility = 'hidden';
-}
-
-
-
-
 
