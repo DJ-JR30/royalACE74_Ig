365945f7da4039eab1e009791db7c31fea96370c
DJ-JR30 <djtddy@hotmail.com>
2021-10-23 03:49:13 -0400

    update v2

--- scripts/Quotes.js
+++ scripts/Quotes.js
@@ -7,6 +7,10 @@ var PageNum = document.getElementById('PageNum');
 var quotesPage1 = document.getElementById('quotesPage1');
 var quotesPage2 = document.getElementById('quotesPage2');
 var quotesPage3 = document.getElementById('quotesPage3');
+var quotesPage4 = document.getElementById('quotesPage4');
+var quotesPage5 = document.getElementById('quotesPage5');
+var quotesPage6 = document.getElementById('quotesPage6');
+var quotesPage7 = document.getElementById('quotesPage7');
 
 
 
@@ -17,34 +21,64 @@ var quotesPage3 = document.getElementById('quotesPage3');
 function PageChanger(Dirction) {
   if(Dirction == 'Back') {
     if(PageNum.innerHTML == 'Page 1') {
-      Page3();
+      Page7();
       return;
     }else{
-      if(PageNum.innerHTML == 'Page 2') {
+    if(PageNum.innerHTML == 'Page 2') {
         Page1();
         return;
-      }else{
-        if (PageNum.innerHTML == 'Page 3') {
-          Page2();
-          return;
-        }
-      }
-    }
-  }else{
-    if(PageNum.innerHTML == 'Page 1') {
-      Page2();
-      return;
     }else{
-      if(PageNum.innerHTML == 'Page 2') {
+    if (PageNum.innerHTML == 'Page 3') {
+        Page2();
+        return;
+    }else{
+    if(PageNum.innerHTML == 'Page 4') {
         Page3();
         return;
-      }else{
-        if (PageNum.innerHTML == 'Page 3') {
-          Page1();
-          return;
-        }
-      }
+    }else{
+    if(PageNum.innerHTML == 'Page 5') {
+        Page4();
+        return;
+    }else{
+    if(PageNum.innerHTML == 'Page 6') {
+        Page5();
+        return;
+    }else{
+    if(PageNum.innerHTML == 'Page 7') {
+      Page6();
+      return;
     }
+    }}}}}}
+  }else{
+    if(PageNum.innerHTML == 'Page 1') {
+        Page2();
+        return;
+    }else{
+    if(PageNum.innerHTML == 'Page 2') {
+        Page3();
+        return;
+    }else{
+    if (PageNum.innerHTML == 'Page 3') {
+        Page4();
+        return;
+    }else{
+    if(PageNum.innerHTML == 'Page 4') {
+        Page5();
+        return;
+    }else{
+    if(PageNum.innerHTML == 'Page 5') {
+        Page6()
+        return;
+    }else{
+    if(PageNum.innerHTML == 'Page 6') {
+        Page7();
+        return;
+    }else{
+    if(PageNum.innerHTML == 'Page 7') {
+      Page1();
+      return;
+    }
+    }}}}}}
   }
 }
 
@@ -73,6 +107,34 @@ function Page3() {
   quotesPage3.style.visibility = 'visible';
 }
 
+function Page4() {
+  HideAllPages();
+  PageNum.innerHTML = 'Page 4';
+  quotesPage4.style.display = 'block';
+  quotesPage4.style.visibility = 'visible';
+}
+
+function Page5() {
+  HideAllPages();
+  PageNum.innerHTML = 'Page 5';
+  quotesPage5.style.display = 'block';
+  quotesPage5.style.visibility = 'visible';
+}
+
+function Page6() {
+  HideAllPages();
+  PageNum.innerHTML = 'Page 6';
+  quotesPage6.style.display = 'block';
+  quotesPage6.style.visibility = 'visible';
+}
+
+function Page7() {
+  HideAllPages();
+  PageNum.innerHTML = 'Page 7';
+  quotesPage6.style.display = 'block';
+  quotesPage6.style.visibility = 'visible';
+}
+
 
 
 
@@ -88,6 +150,18 @@ function HideAllPages() {
   
   quotesPage3.style.display = 'none';
   quotesPage3.style.visibility = 'hidden';
+  
+  quotesPage4.style.display = 'none';
+  quotesPage4.style.visibility = 'hidden';
+  
+  quotesPage5.style.display = 'none';
+  quotesPage5.style.visibility = 'hidden';
+  
+  quotesPage6.style.display = 'none';
+  quotesPage6.style.visibility = 'hidden';
+  
+  quotesPage7.style.display = 'none';
+  quotesPage7.style.visibility = 'hidden';
 }
 
 
--- scripts/index.js
+++ scripts/index.js
@@ -11,6 +11,10 @@ var PageNum = document.getElementById('PageNum');
 var Quot1 = document.getElementById('quotesPage1');
 var Quot2 = document.getElementById('quotesPage2');
 var Quot3 = document.getElementById('quotesPage3');
+var Quot4 = document.getElementById('quotesPage4');
+var Quot5 = document.getElementById('quotesPage5');
+var Quot6 = document.getElementById('quotesPage6');
+var Quot7 = document.getElementById('quotesPage7');
 
 
 
@@ -60,6 +64,18 @@ function HideALL() {
   
   Quot3.style.display = 'none';
   Quot3.style.visibility = 'hidden';
+  
+  Quot4.style.display = 'none';
+  Quot4.style.visibility = 'hidden';
+  
+  Quot5.style.display = 'none';
+  Quot5.style.visibility = 'hidden';
+  
+  Quot6.style.display = 'none';
+  Quot6.style.visibility = 'hidden';
+  
+  Quot7.style.display = 'none';
+  Quot7.style.visibility = 'hidden';
 }
 
 
